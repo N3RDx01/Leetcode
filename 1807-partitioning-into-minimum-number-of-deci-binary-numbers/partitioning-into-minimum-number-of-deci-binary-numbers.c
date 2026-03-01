20afbeaadd23875764dd49a3e8400c2527b70195
Vikrant Pratap Singh <113366748+N3RDx01@users.noreply.github.com>
2026-03-01 23:04:17 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (1.89%) - LeetSync

--- 1807-partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.c
+++ 1807-partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.c
@@ -1,10 +1,8 @@
 int minPartitions(char* n) {
-    char max_digit = '0';
-    for (int i = 0; n[i] != '\0'; i++) {
-        if (n[i] > max_digit) {
-            max_digit = n[i];
-        }
-        if (max_digit == '9') return 9;
+    char max = '0';
+    for (int i = 0; n[i]; i++) {
+        if (n[i] > max) max = n[i];
+        if (max == '9') return 9;
     }
-    return max_digit - '0';
+    return max - '0';
 }
